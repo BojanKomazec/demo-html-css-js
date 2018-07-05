db691621996758d03dfa0ad61dd28c909e58ad6e
Bojan Komazec <bojan.komazec@gmail.com>
2018-07-05 15:28:55 +0100

    Added Fetch API demo page.

---
+++ js/fetch_demo.js
@@ -0,0 +1,84 @@
+//
+// HTML event callbacks
+//
+
+document.getElementById('button-demo-1').onclick = function() {
+    clearElement('output');
+    initialization();
+}
+
+document.getElementById('button-demo-2').onclick = function() {
+    clearElement('output');
+    demo2();
+}
+
+document.getElementById('button-demo-3').onclick = function() {
+    clearElement('output');
+    demo3();
+}
+
+
+//
+// Core functions
+//
+
+
+function initialization() {
+    log('Using *then-catch* idiom to call fetch API:')
+    let url = 'http://geoip.nekudo.com/api'; // returns body in JSON format
+    fetch(url).then(response => {
+        log(`Response: ${response}`);
+        log(`Response status : ${response.status}`);
+        log(`Response statusText : ${response.statusText}`);
+        log(`Response body : ${response.body}`);
+
+        // response data is JSON so can use json() API to parse it
+        log('Parsing response body as *JSON*:');
+        response.json().then(data => {
+            log(`Response data: ${JSON.stringify(data)}`);
+        });
+    }).catch(e => {
+        log(`Error: ${e.statusText}`);
+    });
+}
+
+async function demo2() {
+    log('Using *async-await* idiom to call fetch API:')
+    try {
+        let response = await fetch('http://geoip.nekudo.com/api');
+        log(`Response status : ${response.status}`);
+        log(`Response statusText : ${response.statusText}`);
+        if (response.status == 200) {
+            try {
+                log('Parsing response body as *JSON*:');
+                let data = await response.json();
+                log(`Response data: ${JSON.stringify(data)}`);
+            } catch (e) {
+                log(`Error: ${e.statusText}`);
+            }
+        }
+    } catch (e) {
+        log(`Error: ${e.statusText}`);
+    }
+}
+
+
+async function demo3() {
+    log('Using *async-await* idiom to call fetch API:')
+    try {
+        let response = await fetch('http://geoip.nekudo.com/api');
+        log(`Response status : ${response.status}`);
+        log(`Response statusText : ${response.statusText}`);
+        if (response.status == 200) {
+            try {
+                log('Parsing response body as *text*:');
+                let data = await response.text();
+                log(`Response data: ${data}`);
+            } catch (e) {
+                log(`Error: ${e.statusText}`);
+            }
+        }
+    } catch (e) {
+        log(`Error: ${e.statusText}`);
+    }
+}
--- js/this_demo.js
+++ js/this_demo.js
@@ -16,12 +16,76 @@ function thisInSubroutineDemo() {
     {
         let o = {
             name : "Bojan",
-            greeting : function () {
-                log(`name = ${name}`);
+            logName : function () {
+                log(`name = ${name}`); // name = 
+            },
+            logName2 : function () { // logName2 is a method
+                log(`name = ${this.name}`); // name = Bojan
+            },
+            delayedLogName : function() {
+                let timer = setTimeout(function() { // this is a subroutine
+                    clearTimeout(timer);
+                    log(`delayedLogName(): name = ${name}`); // name = 
+                    log(`this = ${this}`); // this = [object Window]
+                    // this.logName2(); // Uncaught TypeError: this.logName2 is not a function
+                }, 1000);
+            },
+            delayedLogName2 : function() {
+                let timer = setTimeout(function() {
+                    clearTimeout(timer);
+                    log(`delayedLogName2(): name = ${this.name}`);  // name =  
+                    log(`this = ${this}`);                          // this = [object Window]
+                    // this.logName2(); // Uncaught TypeError: this.logName2 is not a function
+                }, 1000);
+            },
+            delayedLogName3 : function() {
+                let that = this;
+                let timer = setTimeout(function() {
+                    clearTimeout(timer);
+                    log(`delayedLogName3(): name = ${that.name}`);  // name = Bojan
+                    log(`this = ${this}`);                          // this = [object Window]
+                    that.logName2();                                // name = Bojan
+                    // this.logName2(); // Uncaught TypeError: this.logName2 is not a function
+                }, 1000);
+            },
+            delayedLogName4 : function() {
+                let timer = setTimeout(function() {
+                    clearTimeout(timer);
+                    log(`delayedLogName4(): name = ${this.name}`);  // name = Bojan
+                    log(`this = ${this}`);                          // this = [object Object]
+                    this.logName2();                                // name = Bojan
+                }.bind(this), 1000);
+            },            
+            delayedLogName5 : function() {
+                let timer = setTimeout(() => {
+                    clearTimeout(timer);
+                    log(`delayedLogName5(): name = ${name}`);       // name =
+                    log(`this = ${this}`);                          // this = [object Object]
+                    this.logName2();                                // name = Bojan
+                }, 1000);
+            },
+            delayedLogName6 : function() {
+                let timer = setTimeout(() => {
+                    clearTimeout(timer);
+                    log(`delayedLogName6(): name = ${this.name}`);  // name = Bojan
+                    log(`this = ${this}`);                          // this = [object Object]
+                    this.logName2();                                // name = Bojan
+                }, 1000);
             }
         }
 
-        o.greeting;
+        o.logName();
+        o.logName2();
+
+        // in these examples this in the callback is bound to the global object (window)
+        o.delayedLogName();
+        o.delayedLogName2();
+        o.delayedLogName3();
+
+        // in these examples this in the callback is bound to the object
+        o.delayedLogName4();
+        o.delayedLogName5();
+        o.delayedLogName6();
     }
   
 }
