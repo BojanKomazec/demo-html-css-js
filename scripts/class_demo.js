1b22c2f735c1c63d0d09345b3b76ec240065c03b
Bojan Komazec <bojan.komazec@gmail.com>
2018-07-15 22:22:48 +0100

    Added class demo.

---
+++ scripts/class_demo.js
@@ -0,0 +1,35 @@
+//
+// HTML event callbacks
+//
+
+document.getElementById('button-demo1').onclick = function() {
+    clearElement('output');
+    demo1();
+}
+
+//
+// Core functions
+//
+
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    // getter
+    get name() {
+        return this._name;
+    }
+
+    sayName() {
+        log(`My name is ${this._name}.`);
+    }
+}
+
+function demo1() {
+    log("Demo1");
+
+    let person = new Person("Bojan");
+    person.sayName();
+    log(`person.name = ${person.name}`);
+}
