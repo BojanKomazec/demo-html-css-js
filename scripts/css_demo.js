d8f05882038de8d7f09363bbcbde859459dab119
Bojan Komazec <bojan.komazec@gmail.com>
2018-07-22 23:44:43 +0100

    Added examples of display styles for div, p and span elements in CSS demo.

--- scripts/css_demo.js
+++ scripts/css_demo.js
@@ -1,3 +1,5 @@
+console.log('Loading script: ' + document.currentScript.src);
+
 document.addEventListener('DOMContentLoaded', function() {
     // square bracket CSS selector allows matching elements by any (custom) attribute
     const elementsToLocalize = document.querySelectorAll('[i16n]');
